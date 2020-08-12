df0147db5098f66a3cecb4fcdbe45bb2dbe6be0e
Hyoin Jeong <hyooin.jeong@gmail.com>
2020-08-12 22:25:07 +0900

    Rename

--- Programmers/서울에서_김서방_찾기.java
+++ Programmers/서울에서_김서방_찾기.java
@@ -2,7 +2,7 @@ package algorithm.Programmers.Lv1;
 
 import java.util.Arrays;
 
-public class 서울에서_김서방_칮기 {
+public class 서울에서_김서방_찾기 {
     public static void main(String[] args) {
         // Test case
         String seoul[] = {"Jane", "Kim"};
